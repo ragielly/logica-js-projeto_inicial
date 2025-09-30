3d099b58dc14bf88f9bb6ac1096fbe436a0223b5
ragielly <ragielly.19@gmail.com>
2025-09-30 17:52:11 -0300

    ajuste no jogo adicionando concatenação

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
  alert('Boas-vindas ao jogo do número secreto')
  let chute = prompt('Escolha um número de 1 e 10')
 
- let numeroSecreto = 4
+ let numeroSecreto = 10
+ console.log(numeroSecreto)
 
  if (chute == numeroSecreto) {
-    alert('Acertou')
+    alert(`Acertou o numero secreto é ${numeroSecreto}` )
  }else{
     alert('Errooou!!');
  }
